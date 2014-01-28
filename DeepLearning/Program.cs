2d55f01686fb5f66b42aaee9ec2bf9519053757f
K <kbecker@primaryobjects.com>
2014-01-28 13:38:04 -0500

    Added comments.

--- DeepLearning/Program.cs
+++ DeepLearning/Program.cs
@@ -107,6 +107,11 @@ namespace DeepLearning
 
         #region Utility Methods
 
+        /// <summary>
+        /// Converts a numeric output label (0, 1, 2, 3, etc) to its cooresponding array of doubles, where all values are 0 except for the index matching the label (ie., if the label is 2, the output is [0, 0, 1, 0, 0, ...]).
+        /// </summary>
+        /// <param name="label">double</param>
+        /// <returns>double[]</returns>
         private static double[] FormatOutputVector(double label)
         {
             double[] output = new double[10];
@@ -126,6 +131,11 @@ namespace DeepLearning
             return output;
         }
 
+        /// <summary>
+        /// Finds the largest output value in an array and returns its index. This allows for sequential classification from the outputs of a neural network (ie., if output at index 2 is the largest, the classification is class "3" (zero-based)).
+        /// </summary>
+        /// <param name="output">double[]</param>
+        /// <returns>double</returns>
         private static double FormatOutputResult(double[] output)
         {
             return output.ToList().IndexOf(output.Max());
